f26912674c387f881962bd3906dadec88403f4a9
Sirine <sirine.khelifi1@outlook.com>
2021-01-13 23:00:32 -0500

    added circles

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 // @TODO: YOUR CODE HERE!
 var svgWidth = 1000;
-var svgHeight = 800;
+var svgHeight = 600;
 var margin = {
-    top: 50,
-    right: 50,
+    top: 90,
+    right: 100,
     bottom: 50,
-    left: 50
+    left: 100
   };
   
   // chart area minus margins
@@ -16,7 +16,7 @@ var svg = d3.select("#scatter")
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight)
-    .attr("transform", "translate(50,100)");
+    .attr("transform", "translate(10,100)");
 
 d3.csv("data/data.csv").then (function(dataInfo) {
 
@@ -40,21 +40,36 @@ d3.csv("data/data.csv").then (function(dataInfo) {
         .domain([0, d3.max(dataInfo, d => d.smokes)])
         .range([svgHeight, 0]);
 
+    var xAxis = d3.axisBottom(xScale);
+    var yAxis = d3.axisLeft(yScale);
+
+    svg.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("transform", "translate(10,-140)")
+        .call(yAxis);
+
     
       // Append a path element to the svg, make sure to set the stroke, stroke-width, and fill attributes.
-    svg.append("g")
-        .selectAll("dot")
+    svg.selectAll("circle")
         .data(dataInfo)
         .enter()
         .append("circle")
         .attr("cx", function (d) { return xScale(d.income); } )
         .attr("cy", function (d) { return yScale(d.smokes); } )
-        .attr("r", function (d) { return d.r;})
+        .attr("r", function (d) { return Math.log(d.income) * 30;})
+        .attr("stroke", "black")
         .style("fill", "#69b3a2")
-
-    svg.append("text").text(function(d) { return dataInfo(d.abbr); })
-        .attr("x", function (d) { return xScale(d.x); })
-        .attr("y", function (d) { return yScale(d.y); });
+        .append("text").text(function(d) { 
+        //console.log(d.abbr)
+    
+        return xScale(d.abbr);
+        
+     })
+        // .attr("x", function (d) { return xAxis(d.income); })
+        // .attr("y", function (d) { return yAxis(d.smokes); });
     
     }).catch(function(error) {
       console.log(error);
